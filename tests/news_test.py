f5093a6a7154492f8651a221e1e98ce394ec2450
Wachira Beatrice <BeatriceWambui>
2019-09-09 00:55:07 +0300

    move news test to test folder

--- tests/news_test.py
+++ tests/news_test.py
@@ -1,6 +1,5 @@
 import unittest
-from models import news
-News = news.News
+from app.models import News
 
 class NewsTest(unittest.TestCase):
     '''
@@ -12,7 +11,7 @@ class NewsTest(unittest.TestCase):
         Set up method that will before every Test
         '''
         self.new_news = News(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993')
-        def test_instance(self):
-            self.assertTrue(isinstance(self.new_news,News))
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
 if __name__ == '__main__':
     unittest.main()
