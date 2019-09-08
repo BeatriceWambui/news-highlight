9c350bc55d1e472fb02aae5935457a689ab94974
Wachira Beatrice <BeatriceWambui>
2019-09-09 00:23:04 +0300

    create a main file and move erros forms and viws files into it

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
+
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
     pass
     NEWS_API_BASE_URL = 'https://api.thenewsdb.org/3/news/{}?api_key={}'
-
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 class ProdConfig(Config):
     '''
     Production configuration child class
@@ -21,3 +25,7 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
     DEBUG = True
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
